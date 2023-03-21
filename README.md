87a2236c3e560b3d597be2095fbdb81db8df3432
Victor Dittiere <fuzip@macbook-pro-fuzip.home>
2023-03-21 23:38:29 +0100

    Initialize project

---
+++ README.md
@@ -0,0 +1,5 @@
+# MultiversX dApp
+
+This app is a very simple decentralized application that will allow the user to deposit a specific number of tokens (default is 1 xEGLD) to a smart contract address and to lock them for a specific amount of time (default is 10 minutes). After this time interval passes, the user can claim back the same amount of tokens. Sending funds to the contract is called ping. Claiming the same amount back is called pong.
+
+See complete tutorial on https://docs.multiversx.com/developers/tutorials/your-first-dapp
---
+++ dapp/src/config.tsx
@@ -0,0 +1,15 @@
+export const contractAddress =
+  'erd1qqqqqqqqqqqqqpgqykhsey926z997r3py8mt289vl70r0uxhtksqapnng2';
+
+export const dAppName = 'example Dapp';
+
+// Generate your own WalletConnect 2 ProjectId here: https://cloud.walletconnect.com/app
+export const walletConnectV2ProjectId = '9b1a9564f91cb659ffe21b73d5c4e2d8';
+
+export const apiTimeout = 6000;
+export const transactionSize = 15;
+export const TOOLS_API_URL = 'https://tools.multiversx.com';
+/**
+ * Calls to these domains will use `nativeAuth` Baerer token
+ */
+export const sampleAuthenticatedDomains = [TOOLS_API_URL];
